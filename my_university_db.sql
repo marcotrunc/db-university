450c7c33d26cef5f61966708a0f1b54ade5ab288
marcotrunc <marcotrunc@gmail.com>
2022-03-09 17:29:02 +0100

    completed second part

--- my_university_db.sql
+++ my_university_db.sql
@@ -36,3 +36,27 @@ FROM `departments`;
 SELECT COUNT(*) 
 FROM `teachers` 
 WHERE `phone` IS NULL;
+
+--! ----------------------------------------------------------------------------------------------------
+--! GROUP BY --
+--! ----------------------------------------------------------------------------------------------------
+
+-- Contare quanti iscritti ci sono stati ogni anno --
+SELECT COUNT(*), YEAR(`enrolment_date`) AS `new_students` 
+FROM `students` 
+GROUP BY `new_students`;
+
+-- Contare gli insegnanti che hanno l'ufficio nello stesso edificio --
+SELECT COUNT(*), `office_address`
+FROM `teachers` 
+GROUP BY `office_address`;
+
+-- Calcolare la media dei voti di ogni appello d'esame -- 
+SELECT ROUND(AVG(`vote`), 2), `exam_id` 
+FROM `exam_student` 
+GROUP BY `exam_id`;
+
+-- Contare quanti corsi di laurea ci sono per ogni dipartimento -- 
+SELECT COUNT(*), `department_id` 
+FROM `degrees` 
+GROUP BY `department_id`;
